d63c6d30d7b3b232c83c4629073ff855fa013443
RB-Alvarez <52293705+RB-Alvarez@users.noreply.github.com>
2026-02-23 21:59:44 -0500

    Main Menu added, Updated MenuManager
    - Colored the buttons
    - Added Main Menu screen
    - Updated MenuManager to allow for separate managements of UI element groups and Canvas screens

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,20 +1,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Menu refers to a group of UI elements
+// Canvas refers to the parent GameObject that holds multiple Menus
+
 public class MenuManager : MonoBehaviour
 {
+    public GameObject[] cavases; //will hold references to all canvas GameObjects, if needed for additional functionality
+    public GameObject initialCanvas; //the canvas that should be active at the start, if needed for additional functionality
     public GameObject[] menus; //will hold references to all menu GameObjects
-    public GameObject initalMenu; //the menu that should be active at the start
+    public GameObject initialMenu; //the menu that should be active at the start
+    
 
     void Start()
     {
-        initalMenu.SetActive(true); // Activate the initial menu
+        initialCanvas.SetActive(true); // Activate the initial canvas
+        foreach (GameObject canvas in cavases)
+        {
+            if (canvas != initialCanvas)
+            {
+                canvas.SetActive(false); // Deactivate all other canvases
+            }
+        }
+
+        initialMenu.SetActive(true); // Activate the initial menu
         foreach (GameObject menu in menus)
         {
-            if (menu != initalMenu)
+            if (menu != initialMenu)
             {
                 menu.SetActive(false); // Deactivate all other menus
             }
+        }
+    }
+
+    // Open a canvas by activating it and deactivating all others
+    public void OpenCanvas(GameObject openThisCanvas)
+    {
+        if (openThisCanvas == null)
+        {
+            return;
+        }
+        foreach (GameObject canvas in cavases)
+        {
+            canvas.SetActive(canvas == openThisCanvas); // Activate only the selected canvas
         }
     }
 
